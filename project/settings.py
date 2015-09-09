5e1788194658bc5cc9094d4ba5d88ec7d6b4e63f
Marta Alonso <marta.alonso.fernandez@gmail.com>
2015-09-09 12:40:38 -0300

    Changed configuration for CABA

--- project/settings.py
+++ project/settings.py
@@ -60,7 +60,7 @@ MANAGERS = ADMINS
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Europe/Madrid'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 # Location of translation files (used by themes to override certain strings)
 LOCALE_PATHS = (
@@ -72,6 +72,7 @@ LOCALE_PATHS = (
 if not 'LANGUAGES' in locals():
     # All choices can be found here: http://www.i18nguy.com/unicode/language-identifiers.html
     LANGUAGES = (
+      ('es-AR', 'Castellano'),
       ('es-ES', 'Castellano'),
     )
 
